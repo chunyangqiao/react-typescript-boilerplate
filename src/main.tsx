87dc19ddcfbbffc136be119929c7c67c328f0aee
chunyangqiao <chunyangqiao@yzw.cn>
2023-03-08 00:13:23 +0800

    chore: initial

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
